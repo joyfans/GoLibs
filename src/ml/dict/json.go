8008fbc107b455c8b2705387d796502f268ab945
unknown <Hiromi.Kaede@gmail.com>
2016-02-28 21:22:07 +0800

    improve JsonDict and JsonArray

--- src/ml/dict/json.go
+++ src/ml/dict/json.go
@@ -3,38 +3,53 @@ package dict
 type JsonDict map[string]interface{};
 type JsonArray []interface{};
 
-func (self JsonDict) Map(key string) JsonDict {
-    i := self[key]
-    if i == nil {
-        return nil
-    }
+func toJsonDict(v interface{}) JsonDict {
+    switch t := v.(type) {
+        case nil:
+            return nil
 
-    return i.(map[string]interface{})
-}
+        case JsonDict:
+            return t
 
-func (self JsonDict) Array(key string) JsonArray {
-    i := self[key]
-    if i == nil {
-        return nil
-    }
-
-    return i.([]interface{})
-}
-
-func (self JsonArray) Map(index int) JsonDict {
-    v := self[index]
-    if v == nil {
-        return nil
+        case *JsonDict:
+            return *t
     }
 
     return v.(map[string]interface{})
 }
 
-func (self JsonArray) Array(index int) JsonArray {
-    v := self[index]
-    if v == nil {
-        return nil
+func toJsonArray(v interface{}) JsonArray {
+    switch t := v.(type) {
+        case nil:
+            return nil
+
+        case JsonArray:
+            return t
+
+        case *JsonArray:
+            return *t
     }
 
     return v.([]interface{})
+}
+
+func (self JsonDict) Map(key string) JsonDict {
+    return toJsonDict(self[key])
+}
+
+func (self JsonDict) Array(key string) JsonArray {
+    return toJsonArray(self[key])
+}
+
+func (self JsonArray) Map(index int) JsonDict {
+    return toJsonDict(self[index])
+}
+
+func (self JsonArray) Array(index int) JsonArray {
+    return toJsonArray(self[index])
+}
+
+func (self *JsonArray) Append(values ...interface{}) *JsonArray {
+    *self = append(*self, values...)
+    return self
 }
