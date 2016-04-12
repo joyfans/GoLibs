b335bd08e2e42461626587f5058c8470cd3dd66e
unknown <Hiromi.Kaede@gmail.com>
2016-04-12 22:52:07 +0800

    add inke to GOPATH

--- src/tools/init.bat
+++ src/tools/init.bat
@@ -1,5 +1,5 @@
 set GOROOT=%~dp0
-set GOPATH=%~dp0pkgs;D:\Desktop\Source\GoProject;D:\Desktop\Source\Project\Private;D:\Desktop\Source\Falcom
+set GOPATH=%~dp0pkgs;D:\Desktop\Source\GoProject;D:\Desktop\Source\Project\Private;D:\Desktop\Source\Falcom;D:\Desktop\Source\GoProject\src\inke_client
 set PATH=%GOROOT%bin;%GOROOT%pkgs\bin;D:\Dev\PortableGit\bin;D:\Desktop\Source\GoProject\src\AppleIdRegister;%PATH%
 set GOGCCFLAGS="-g -O3 -fPIC -m64 -pthread --gc-sections"
 set CGO_ENABLED="1"
